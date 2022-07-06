7918106d4af25a48fe0dfa62ef3b464bec075361
Romina Rodes <104787916+RominaRodes@users.noreply.github.com>
2022-07-06 12:48:21 -0300

    hme de prueba y detail casi dimanico

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,9 +1,21 @@
-const path =require('path')
+const path = require('path');
+const fs = require('fs');
+const dataPath = path.join(__dirname,'../database/jugetes.json');
+const data =JSON.parse(fs.readFileSync(dataPath,"UTF-8"));
 
-const mainController = {
+module.exports = {
     index : (req, res) => {
-        res.render('home'); // home ejs
+        let categorias= 
+            [{categoria: 'Sensoriales', img:"/img/categoria-6meses.png"},
+            {categoria: 'Instrumentos Musicales', img:"/img/categoria-6meses.png"},
+            {categoria: 'Rompecabezas', img:"/img/categoria-6meses.png"},
+            {categoria: 'Movimientos', img:"/img/categoria-6meses.png"} ,
+            {categoria: 'Bloques', img:"/img/pizzaras.png"}
+
+        ]         //en n futuro agregar a base de datos
+        
+
+        res.render('pruebaHome', {data, categorias}); // prueba de pagina home dinamica
+        //res.render('home', {data}); // home ejs
     }
 }
-
-module.exports = mainController;
