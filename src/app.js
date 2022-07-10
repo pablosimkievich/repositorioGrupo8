1a4b2259b49351ee4388f8ee0b24a38d99f11aca
DenuArcuri <denn.arcuri@gmail.com>
2022-07-10 00:40:41 -0300

    update de CSS

--- src/app.js
+++ src/app.js
@@ -10,27 +10,19 @@ const methodOverride = require('method-override');
 app.use(express.static(path.join(__dirname,'../public')));
 
 app.set('view engine', 'ejs'); // motor de plantillas ejs
-
 app.set('views','src/views');   // Para no tener que escribir toda la ruta
-
 app.use(morgan('dev'));                  //muestar mas info de errores  
-
 app.use(methodOverride('_method'));        //permite editar datos
-
 app.use(express.urlencoded({ extended: false }));  //capturainfo formulario
-
 app.use(express.json()); // y si queremos lo pasa json
 
 app.use('/', mainRouter);
-
-app.use( userRouter);
-
-app.use( productRouter);
+app.use(userRouter);
+app.use(productRouter);
 
 
 
 app.set('puerto',process.env.PORT || 3000)
-
 app.listen(app.get('puerto'), ()=>console.log(`Servidor escuchando en puerto ${app.get('puerto')}`));
 
 
