8ee9b6f47c0e4b1e7599a667b488ba0892082197
pablosimkievich <pablosimkievich@gmail.com>
2022-07-14 23:23:01 -0300

    contenido dinamico con map a home.ejs, ajusto banner en home-styles.css y modifico maincontroller para contenido dinamico en home (index)

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -5,40 +5,8 @@ const data =JSON.parse(fs.readFileSync(dataPath,"UTF-8"));
 
 module.exports = {
     index : (req, res) => {
-       let arrayMock = [
-        {
-            nombre: "Banco didáctico",
-            precio: "$ 4800",
-            cuotas: "3 cuotas de $1600"
-        },
-        {
-            nombre: "Cocodrilo de arrastre",
-            precio: "$ 3200",
-            cuotas: "3 cuotas de $1066"
-        },
-        {
-            nombre: "Cohete multifunción",
-            precio: "$ 7900",
-            cuotas: "3 cuotas de $2630"
-        },
-        {
-            nombre: "Mesa de expresión",
-            precio: "$ 4500",
-            cuotas: "3 cuotas de $1500"
-        },
-        {
-            nombre: "Torre geométrica",
-            precio: "$ 1600",
-            cuotas: "3 cuotas de $530"
-        },
-        {
-            nombre: "Carra andador",
-            precio: "$ 2500",
-            cuotas: "3 cuotas de $830"
-        }
-       ]
-
-        res.render('home', {arrayMock, arrayMock}); // home ejs 
+       
+        res.render('home', {data, data}); // home ejs
     },
     indexCategorias: (req, res) => {
      
