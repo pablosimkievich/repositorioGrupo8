cd74947e7f3187b01adf9d3ea472a911e85f2277
pablosimkievich <pablosimkievich@gmail.com>
2022-09-02 16:55:26 -0300

    incorporo cambios front-end de Denn avatar header footer, corrijo un error

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": '',
-    "database": "test",
+    "database": "rayuela",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
