af3fec40302f29d9ae7458f7e88fc3cccdf45c32
Romina Rodes <104787916+RominaRodes@users.noreply.github.com>
2022-09-29 20:20:23 -0300

    carpeta js en public y modificaciones a las estrellas de review form

---
+++ public/js/pintarEstrellas.js
@@ -0,0 +1,16 @@
+
+
+window.addEventListener('load',function(){
+    console.log('hola')
+   let inputRating = document.querySelector('#rating-input')
+   let estrellas = document.querySelector('.stars-inner')
+          
+     inputRating.addEventListener('change', ()=>{
+
+        
+
+           })
+            
+        
+
+  })
