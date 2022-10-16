cf5ba750f1409caec1cd3afdd9f4605642af23bc
Romina Rodes <104787916+RominaRodes@users.noreply.github.com>
2022-10-16 16:01:17 -0300

    solucion conflicto ruta de las ordenes de compra

--- src/routes/api/apiUserRouter.js
+++ src/routes/api/apiUserRouter.js
@@ -2,13 +2,8 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../../controllers/api/userController');
 
-<<<<<<< HEAD:src/routes/apiRouter/apiUserRouter.js
 router.get('/users', userController.getUsers);
 router.get('/users/:id', userController.userDetail);
 router.get('/orders', userController.getOrders)
-=======
-router.get('/api/users', userController.getUsers);
-router.get('/api/users/:id', userController.userDetail);
->>>>>>> 7e6f7154777e039bbbd7452f330405e09340e4a9:src/routes/api/apiUserRouter.js
 
 module.exports = router;
