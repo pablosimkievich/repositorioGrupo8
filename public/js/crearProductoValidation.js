30c19a48007b36c1479916d37faaaef36723d972
Romina Rodes <104787916+RominaRodes@users.noreply.github.com>
2022-10-19 23:03:27 -0300

    validaciones de crear producto

--- public/js/crearProductoValidation.js
+++ public/js/crearProductoValidation.js
@@ -1,43 +1,51 @@
 window.addEventListener("load", function() {
+console.log('soy js')
+let buttonform = document.querySelector('#form-button');
 
-const buttonform = document.querySelector('#form-button');
-const form =  document.querySelector('.create-form');
 
 buttonform.addEventListener('click', (e) => {
- console.log('hola click')
-e.preventDefault()
-
-const name = document.getElementById('input-name');
-const description = document.querySelector('.textarea');
-const img = document.getElementById('ppal-img-input');
-
-let errores =[];
-let erroresUl = document.querySelector('div.erroresFront ul');
-
-
-while (erroresUl.firstChild) {
+  console.log('click')
+ 
+  let errores = [];
+  let erroresUl = document.querySelector('div.erroresFront ul');
+  while (erroresUl.firstChild) {
     erroresUl.removeChild(erroresUl.firstChild);
 };
 
+ 
+
+const name = document.querySelector('#input-name');
+const description = document.querySelector('.textarea');
+const img = document.getElementById('ppal-img-input');
+
+
+
+
+
+
 if(name.value == ''){
-    errores.push('Debes completar este campo')
-    
+    errores.push('Debes completar el nombre')
+       
   }
 if(name.value.length < 5){
-    errores.push("El titulo tiene que tener al menos 5 caracteres")
+    errores.push("El nombre tiene que tener al menos 5 caracteres")
   }
-  console.log('hice click')
+
+  if(description.value.length <20){
+    errores.push("La descripcion tiene que tener al menos 20 caracteres")
+  }
+
+
+
 
 if(errores.length>0){
     e.preventDefault();
     let erroresUl = document.querySelector('div.erroresFront ul')
-    console.log(erroresUl)
+    
   errores.map(element=>{
-    erroresUl.innerHTML += "<li >" + element + "</li>"
+    erroresUl.innerHTML += "<li>" + element + "</li>"
   })
 
-}else{
-  form.submit()
 }
 
 
--- src/controllers/api/userController.js
+++ src/controllers/api/userController.js
@@ -4,13 +4,24 @@ const { name } = require('ejs');
 
 const getUsers = async (_req, res) => {
     try {
-        const allUsers = await db.User.findAll()
+        const allUsers = await db.User.findAll( {include: [
+            {
+                association: 'order_detail'
+            },
+            {
+                association: 'reviews'
+            }
+        ]})
         const users = allUsers.map(e =>{
             return {
                 id: e.id,
                 name: e.user_first_name + ' ' + e.user_last_name,
                 email: e.user_mail,
-
+                cel: e.user_cel,
+                address: e.user_address,
+                img: `http://localhost:3001/../../../img/users/${e.user_img}`,
+                dni: e.dni,
+                reviews: e.reviews,
                 detail: `http://localhost:3001/api/users/${e.id}`
                 }
         })
@@ -62,12 +73,30 @@ const userDetail = async (req, res) => {
     const getOrders = async (_req,res) => {
         try {
           
-            const orders = await db.Order.findAll(
+            const apiOrders = await db.Order.findAll(
                 {include: [
-                    {association: 'users'}     
-                    ]
+                    {association: 'users'},
+                    {association: 'payment_method'},
+                    {association: 'order_detail'}     
+                    ],
+                order: [
+                    ['order_date', 'ASC']
+                ]
             }
-            );
+            )
+            const orders = apiOrders.map(e => {
+                return {
+                    id: e.id,
+                    user_id: e.user_id,
+                    order_total_amt: e.order_total_amt,
+                    order_date: e.order_date,
+                    order_status: e.order_status,
+                    payment_method: e.payment_method.pay_method_type,
+                    users: e.users, 
+                    order_detail: e.order_detail
+                }
+            });
+
             const count = await db.Order.count();
             const totalSales = await db.Order.sum('order_total_amt')
 
