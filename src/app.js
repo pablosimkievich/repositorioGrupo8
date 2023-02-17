11812fbd9141a37b31e39d835ae0ea6f18b8b81e
pablosimkievich <pablosimkievich@gmail.com>
2023-02-17 20:48:08 -0300

    db ports + api port

--- src/app.js
+++ src/app.js
@@ -52,9 +52,9 @@ app.use( (req,res,next) => {
     res.status(404).render('not-found-404');
 })
 
+// config para railway
+
 const puerto = process.env.PORT;
-
-
 
 app.set('puerto', process.env.PORT || 3001)
 app.listen(app.get('puerto'), ()=>console.log(`Servidor escuchando en puerto ${app.get('puerto')}`));
