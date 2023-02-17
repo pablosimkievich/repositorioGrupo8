f728824ee25e28e1ee49dcc237ba4128908a4c4b
pablosimkievich <pablosimkievich@gmail.com>
2023-02-16 23:44:07 -0300

    db config

--- src/app.js
+++ src/app.js
@@ -55,11 +55,11 @@ app.use( (req,res,next) => {
 const puerto = process.env.PORT;
 
 const dbConfig = {
-    host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || "3306",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "rayuela",
+    host: process.env.HOST || "localhost",
+    db_port: process.env.DB_PORT || "3306",
+    user: process.env.USER || "root",
+    password: process.env.PASSWORD || "",
+    database: process.env.DATABASE || "rayuela",
   };
   
 app.use(conn(mysql, dbConfig, "single"));
