f0b1b07e4ddfe57cf6053a8269a10c34921fb228
pablosimkievich <pablosimkievich@gmail.com>
2023-02-22 14:15:38 -0300

    undo sequelize config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
+    "username": "root",
+    "password": '',
+    "database": "rayuela",
+    "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
