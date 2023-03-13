11ea7616f5a961ce8ad4af305c35d9ef71a77c0e
pablosimkievich <105614947+pablosimkievich@users.noreply.github.com>
2023-03-13 00:00:46 -0300

    Update config.js
    update host sequelize config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -3,7 +3,7 @@ module.exports = {
     "username": "root",
     "password": '',
     "database": "rayuela",
-    "host": "127.0.0.1",
+    "host": "0.0.0.0:6745",
     "dialect": "mysql"
   },
   "test": {
@@ -17,7 +17,7 @@ module.exports = {
     "username": "root",
     "password": null,
     "database": "database_production",
-    "host": "127.0.0.1",
+    "host": "0.0.0.0:6745",
     "dialect": "mysql"
   }
 }
