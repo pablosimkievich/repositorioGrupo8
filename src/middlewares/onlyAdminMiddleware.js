009cbd637172dd76e15103c965814d55c2f6f218
pablosimkievich <105614947+pablosimkievich@users.noreply.github.com>
2023-03-14 14:18:39 -0300

    Update onlyAdminMiddleware.js
    activo onlyAdminMiddleware.js

--- src/middlewares/onlyAdminMiddleware.js
+++ src/middlewares/onlyAdminMiddleware.js
@@ -1,14 +1,14 @@
-// ! middleware funciona, desactivado por fines prácticos
+
 // ? Este middleware, en caso de que un usuario no logueado quiera entrar a las páginas del admin, es 
 // ? redirijido a home. En caso de estar logueado pero no ser admin, también es redirijido a home.
 
 function onlyAdminMiddleware (req, res, next) {
-/*    if (!req.session.userLogged) {
+    if (!req.session.userLogged) {
         return res.redirect('/')
         
     } else if (req.session.userLogged.user_type_id !== 2){ // ? user_type_id == 2 // es el admin
         return res.redirect('/')
-    } */
+    } 
 
     next();
 };
