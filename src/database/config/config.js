59e73d8c9e626ea5aec7e497ed2f423ced2b85cc
pablosimkievich <105614947+pablosimkievich@users.noreply.github.com>
2023-04-20 00:12:33 -0300

    Update config.js
    regenerate

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,31 +1,38 @@
 module.exports = {
   "development": {
-    "username": "bg4slhkz682jeytpnwpd",
-    "password": "pscale_pw_mLXpAy7ZYzkEg5rkSNBIcw0WBhQn5b4ZzcReCN6sUjR",
+    "username": "m32faj3pufs7fjtkvor3",
+    "password": "pscale_pw_daKAOf39GYekSgy6CKRCdchFwtlfmdQmSSulxoBQ85r
+",
     "database": "rayuela",
     "host": "aws.connect.psdb.cloud",
     "port": "3306", 
     "dialect": "mysql",
     "ssl": {
-      "rejectUnauthorized": "false"
+      "rejectUnauthorized": false
     }
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": "m32faj3pufs7fjtkvor3",
+    "password": "pscale_pw_daKAOf39GYekSgy6CKRCdchFwtlfmdQmSSulxoBQ85r
+",
+    "database": "rayuela",
+    "host": "aws.connect.psdb.cloud",
+    "port": "3306",
+    "dialect": "mysql",
+      "ssl": {
+      "rejectUnauthorized": false
+    }
   },
   "production": {
-    "username": "bg4slhkz682jeytpnwpd",
-    "password": "pscale_pw_mLXpAy7ZYzkEg5rkSNBIcw0WBhQn5b4ZzcReCN6sUjR",
+    "username": "m32faj3pufs7fjtkvor3",
+    "password": "pscale_pw_daKAOf39GYekSgy6CKRCdchFwtlfmdQmSSulxoBQ85r
+",
     "database": "rayuela",
     "host": "aws.connect.psdb.cloud",
     "port": "3306",
     "dialect": "mysql",
         "ssl": {
-      "rejectUnauthorized": "false"
+      "rejectUnauthorized": false
     }
   }
 }
