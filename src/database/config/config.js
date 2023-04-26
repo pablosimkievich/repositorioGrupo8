9cfd59bc8b53998932ce18f7dc34559500a5c491
pablosimkievich <105614947+pablosimkievich@users.noreply.github.com>
2023-04-25 22:55:05 -0300

    Update config.js
    G

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -5,14 +5,20 @@ module.exports = {
     "database": "lucas_rayuela",
     "host": "138.128.163.34",
     /* "port": "6745", */
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "ssl": {
+      "rejectUnauthorized": false
+    }
   },
   "test": {
     "username": "rayuela_user",
     "password": "k_VS=GEDRr?%",
     "database": "lucas_rayuela",
     "host": "138.128.163.34",
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "ssl": {
+      "rejectUnauthorized": false
+    }
   },
   "production": {
     "username": "rayuela_user",
@@ -20,6 +26,9 @@ module.exports = {
     "database": "lucas_rayuela",
     "host": "138.128.163.34",
     /* "port": "6745", */
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "ssl": {
+      "rejectUnauthorized": false
+    }
   }
 }
