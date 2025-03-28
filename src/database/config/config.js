111970392f58bf745157589cde0d7985fd36ac8c
pablosimkievich <105614947+pablosimkievich@users.noreply.github.com>
2025-03-28 14:53:56 -0300

    process env

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,34 +1,34 @@
 module.exports = {
   "development": {
-    "username": "sql5754365",
-    "password": "LFALEV3tDB",
-    "database": "sql5754365",
-    "host": "sql5.freesqldatabase.com",
-    "port": 3306,
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT || 3306,
     "dialect": "mysql",
-      "ssl": {
+    "ssl": {
       "rejectUnauthorized": false
     }
-    },
+  },
   "test": {
- "username": "sql5754365",
-    "password": "LFALEV3tDB",
-    "database": "sql5754365",
-    "host": "sql5.freesqldatabase.com",
-    "port": 3306,
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT || 3306,
     "dialect": "mysql",
-      "ssl": {
+    "ssl": {
       "rejectUnauthorized": false
     }
-     },
+  },
   "production": {
-    "username": "sql5754365",
-    "password": "LFALEV3tDB",
-    "database": "sql5754365",
-    "host": "sql5.freesqldatabase.com",
-    "port": 3306,
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT || 3306,
     "dialect": "mysql",
-      "ssl": {
+    "ssl": {
       "rejectUnauthorized": false
     }
   }
